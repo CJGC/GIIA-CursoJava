1555e667116e2b98b2b1dc5ceb927d1ddf65bad3
cjgc <chjugomez@utp.edu.co>
2018-07-13 12:27:21 -0500

    Homework 3 added into main method

--- week1/homeworks/src/homeworks/Homeworks.java
+++ week1/homeworks/src/homeworks/Homeworks.java
@@ -10,13 +10,31 @@ public class Homeworks {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        NumbersWithout2and5 numbersWithout2and5 = new NumbersWithout2and5();
+        NumbersWithout2and5 numbersWithout2and5 = new NumbersWithout2and5(1,100);
         EvenNumbers evenNumbers = new EvenNumbers(1539,2505);
         
+        // ------ homework 1 ------
         numbersWithout2and5.printNumbers();
         System.out.println();
+        
+        // ------ homework 2 ------
         evenNumbers.printEvenNumbers();
         System.out.println();
+        
+        // ------ homework 3 ------
+        if(args.length != 2) {
+            System.err.println("There must be 2 armuments!");
+            return;
+        }
+        
+        double x = Double.parseDouble(args[0]);
+        double y = Double.parseDouble(args[1]);
+        
+        Arithmetic arithmetic = new Arithmetic(x,y);
+        System.out.println(x+" + "+y+" = " +arithmetic.add());
+        System.out.println(x+" - "+y+" = " +arithmetic.sub());
+        System.out.println(x+" * "+y+" = " +arithmetic.mul());
+        System.out.println(x+" / "+y+" = " +arithmetic.div()); 
     }
     
 }
