1fb9eb2f585ac3221b665edcf6f1a01c7de7ebd2
cjgc <chjugomez@utp.edu.co>
2018-07-13 13:43:46 -0500

    homework 4,5,6 sections were introduced

--- week1/homeworks/src/homeworks/Homeworks.java
+++ week1/homeworks/src/homeworks/Homeworks.java
@@ -1,28 +1,30 @@
 package homeworks;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 /**
  *
  * @author cj
  */
 public class Homeworks {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         NumbersWithoutEnding2and5 numbersWithout2and5 = 
                 new NumbersWithoutEnding2and5(1,100);
-        EvenNumbers evenNumbers = new EvenNumbers(1539,2505);
         
-        // ------ homework 1 ------
+        // ------ homework 1 printing numbers whitout ending in 2 and 5 ------
         numbersWithout2and5.printNumbers();
         System.out.println();
         
-        // ------ homework 2 ------
+        // ------ homework 2 printing even numbers into an interval ------
+        EvenNumbers evenNumbers = new EvenNumbers(1539,2505);
         evenNumbers.printEvenNumbers();
         System.out.println();
         
-        // ------ homework 3 ------
+        // ------ homework 3 using command line arguments ------
         if(args.length != 2) {
             System.err.println("There must be 2 armuments!");
             return;
@@ -35,7 +37,32 @@ public class Homeworks {
         System.out.println(x+" + "+y+" = " +arithmetic.add());
         System.out.println(x+" - "+y+" = " +arithmetic.sub());
         System.out.println(x+" * "+y+" = " +arithmetic.mul());
-        System.out.println(x+" / "+y+" = " +arithmetic.div()); 
+        System.out.println(x+" / "+y+" = " +arithmetic.div());
+        
+        // ------ homework 4 getting user input with BufferedReader ------
+        BufferedReader dataIn = new BufferedReader
+            (new InputStreamReader(System.in));
+        String userInput = "";
+        try {
+            System.out.print("\n(BufferedReader input) Type some text: ");
+            userInput = dataIn.readLine();
+        }
+        catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        
+        System.out.println("\nYou've typed: "+userInput);
+        
+        // ------ homework 5 getting user input with Scanner ------
+        Scanner scanIn = new Scanner(System.in);
+        System.out.print("\n(Scanner input) Type some text: ");
+        userInput = scanIn.nextLine();
+        System.out.println("\nYou've typed: "+userInput);
+        
+        // ------ homework 6 getting user input with swing ------
+        userInput = JOptionPane.showInputDialog("Type some text: ");
+        String msg = "You've type: " + userInput;
+        JOptionPane.showMessageDialog(null,msg);
     }
     
 }
