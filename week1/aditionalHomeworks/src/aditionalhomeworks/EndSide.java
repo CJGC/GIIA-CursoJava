aed4b9762496acd55ea7ca55f13bdced022aae42
cjgc <chjugomez@utp.edu.co>
2018-07-17 12:14:58 -0500

    EndSide interface

---
+++ week1/aditionalHomeworks/src/aditionalhomeworks/EndSide.java
@@ -0,0 +1,12 @@
+package aditionalhomeworks;
+
+/**
+ *
+ * @author cj
+ */
+public interface EndSide {
+    int min (int []array);
+    int max (int []array);
+    double min (double []array);
+    double max (double []array);
+}
