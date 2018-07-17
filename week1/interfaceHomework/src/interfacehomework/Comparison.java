5d0414737730d828290f6e668153453c2515c6d5
cjgc <chjugomez@utp.edu.co>
2018-07-16 19:37:29 -0500

    Comparison interface

---
+++ week1/interfaceHomework/src/interfacehomework/Comparison.java
@@ -0,0 +1,10 @@
+package interfacehomework;
+/**
+ *
+ * @author cj
+ */
+public interface Comparison {
+    public boolean isGreater(Object a);
+    public boolean isLess(Object a);
+    public boolean isEqual(Object a);
+}
