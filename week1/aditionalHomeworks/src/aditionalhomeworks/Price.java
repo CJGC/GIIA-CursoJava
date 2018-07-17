b38559e7f1e9647f80bf71cb105f0e8f4b934682
cjgc <chjugomez@utp.edu.co>
2018-07-17 12:16:53 -0500

    Invoice class

---
+++ week1/aditionalHomeworks/src/aditionalhomeworks/Price.java
@@ -0,0 +1,18 @@
+package aditionalhomeworks;
+
+/**
+ *
+ * @author cj
+ */
+public class Price {
+    public double pesos;
+
+    public double getPesos() {
+        return pesos;
+    }
+
+    public void setPesos(double pesos) {
+        this.pesos = pesos;
+    }
+    
+}
