f0b601a0528f288986d1bb310a0a3d88454c634b
cjgc <chjugomez@utp.edu.co>
2018-07-17 14:07:18 -0500

    Main class

---
+++ week1/ExceptionHexHomework/src/ExeptionHexHomeworks/MainClass.java
@@ -0,0 +1,37 @@
+package ExeptionHexHomeworks;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+/**
+ *
+ * @author cj
+ */
+public class MainClass {
+
+    public static Pattern pattern = Pattern.compile("^[0-9abcdfABCDF]+");
+    public static Matcher matcher;
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.Exception
+     */
+    public static void main(String[] args) throws Exception {
+        if(args.length != 1) {
+            System.err.println("Is needed an argument of type hexadecimal!");
+            return;
+        }
+        
+        matcher = pattern.matcher(args[0]);
+        try {
+            if(!matcher.matches())
+                throw new ExceptionHexToDec();
+        }
+        catch (ExceptionHexToDec e) {
+            System.err.println(e.getMessage() + "\nPlease type a hexadecimal"
+                + "number");
+        }
+        
+        System.out.println("The number " + args[0] + " in the decimal format"
+                + "is = " + Integer.parseInt(args[0],16));
+    }
+    
+}
+
