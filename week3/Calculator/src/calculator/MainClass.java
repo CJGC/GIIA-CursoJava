6c644339680c0841e75fbcdcae76e0b53756ded1
cjgc <chjugomez@utp.edu.co>
2018-07-19 21:31:35 -0500

    Main class

---
+++ week3/Calculator/src/calculator/MainClass.java
@@ -0,0 +1,16 @@
+package calculator;
+
+/**
+ *
+ * @author cj
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Converter conv = new Converter();
+    }
+    
+}
