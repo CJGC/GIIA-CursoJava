e8d905fdf5f1137a0151f32c29e5586ad512b914
cjgc <chjugomez@utp.edu.co>
2018-07-24 13:35:45 -0500

    Class limits

---
+++ week3/Calculator/src/limits/Limits.java
@@ -0,0 +1,14 @@
+package limits;
+/**
+ *
+ * @author cj
+ */
+public class Limits {
+    public final static int displayLenght = 14;
+    public final static int maxIntDigits = 7;
+    public final static int maxDoubleDigits = 6;
+    public final static int maxDecValueAllowed = 9999999;
+    public final static int maxBinValueAllowed = 127;
+    public final static int maxOctValueAllowed = 2097151;
+    public final static int maxHexValueAllowed = 268435455;
+}
