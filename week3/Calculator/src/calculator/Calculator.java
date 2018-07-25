ab220216122b586bbd152c2efd06fe7ca273b4d6
cjgc <chjugomez@utp.edu.co>
2018-07-24 19:29:50 -0500

    Code adjustments

--- week3/Calculator/src/calculator/Calculator.java
+++ week3/Calculator/src/calculator/Calculator.java
@@ -6,9 +6,11 @@ package calculator;
 public class Calculator extends Converter {
     
     private double ANS;
+    private boolean firstOperation;
     
     public Calculator() {
         ANS = 0.0;
+        firstOperation = true;
     }
     
     public double getANS() {
@@ -16,17 +18,19 @@ public class Calculator extends Converter {
     }
 
     public String getANSinStringFormat() {
-        String output = trimOutput(getANS());
-        if(output == "Max val reached!") {
-            setANS(0.0);
-            return output;
-        }
-        setANS(Double.parseDouble(output));
-        return decToBase(output);
+        return decToBase(Double.toString(getANS()));
     }
     
     public void setANS(double ANS) {
         this.ANS = ANS;
+    }
+
+    public boolean isFirstOperation() {
+        return firstOperation;
+    }
+
+    public void setFirstOperation(boolean firstOperation) {
+        this.firstOperation = firstOperation;
     }
     
     public int getBase() {
@@ -37,67 +41,63 @@ public class Calculator extends Converter {
         this.base = base;
     }
     
-    public String add(String value) {
+    private String doOperation(String operator,String value) {
         double finalValue = baseToDec(value);
-        setANS(ANS + finalValue);
-        String output = trimOutput(getANS());
-        if(output == "Max val reached!") {
-            setANS(0.0);
-            return output;
+        if(!isFirstOperation())
+            switch(operator) {
+                case "add":
+                    setANS(getANS() + finalValue);
+                    break;
+                case "sub":
+                    setANS(getANS() - finalValue);
+                    break;
+                case "mul":
+                    setANS(getANS() * finalValue);
+                    break;
+                case "div":
+                    setANS(getANS() / finalValue);
+                    break;
+                case "inv":
+                    setANS(1 / finalValue);
+                    break;
+            }
+        else {
+            setFirstOperation(false);
+            setANS(finalValue);
         }
-        setANS(Double.parseDouble(output));
-        return decToBase(output);
+        
+        String trimmedANS = trimOutput(getANS());
+        if(trimmedANS != "Max val reached!") {
+            trimmedANS = decToBase(trimmedANS);
+            setANS(Double.parseDouble(trimmedANS));
+        }
+        else setANS(0.0);
+        return trimmedANS;
+    }
+    
+    public String add(String value) {
+        return doOperation("add",value);
     }
     
     public String sub(String value) {
-        double finalValue = baseToDec(value);
-        setANS(ANS - finalValue);
-        String output = trimOutput(getANS());
-        if(output == "Max val reached!") {
-            setANS(0.0);
-            return output;
-        }
-        setANS(Double.parseDouble(output));
-        return decToBase(output);
+        return doOperation("sub",value);
     }
     
     public String mul(String value) {
-        double finalValue = baseToDec(value);
-        setANS(ANS * finalValue);
-        String output = trimOutput(getANS());
-        if(output == "Max val reached!") {
-            setANS(0.0);
-            return output;
-        }
-        setANS(Double.parseDouble(output));
-        return decToBase(output);
+        return doOperation("mul",value);
     }
 
     public String div(String value) {
         double finalValue = baseToDec(value);
         if (finalValue == 0)
             return "div by zero!";
-        setANS(ANS / finalValue);
-        String output = trimOutput(getANS());
-        if(output == "Max val reached!") {
-            setANS(0.0);
-            return output;
-        }
-        setANS(Double.parseDouble(output));
-        return decToBase(output);
+        return doOperation("div",value);
     }
     
     public String inv() {
         if (getANS() == 0) 
             return "div by zero!";
-        setANS(1 / ANS);
-        String output = trimOutput(getANS());
-        if(output == "Max val reached!") {
-            setANS(0.0);
-            return output;
-        }
-        setANS(Double.parseDouble(output));
-        return decToBase(output);
+        return doOperation("inv",Double.toString(getANS()));
     }
     
     public void assignment(String value) {
