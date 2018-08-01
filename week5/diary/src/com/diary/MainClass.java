b0956cb5dafe4e4c8a75c326b3840bf4f7e02b62
cjgc <chjugomez@utp.edu.co>
2018-07-31 23:11:59 -0500

    making tables on database

--- week5/diary/src/com/diary/MainClass.java
+++ week5/diary/src/com/diary/MainClass.java
@@ -1,5 +1,5 @@
 package com.diary;
-import com.database.DbConnection;
+import com.database.*;
 /**
  *
  * @author cj
@@ -11,6 +11,20 @@ public class MainClass {
      */
     public static void main(String[] args) {
         DbConnection conn = new DbConnection();
+        ManageTables mt = new ManageTables(conn);
+        mt.createTables(TablesDefinition.countryTable);
+        mt.createTables(TablesDefinition.provinceTable);
+        mt.createTables(TablesDefinition.cityTable);
+        mt.createTables(TablesDefinition.addressTable);
+        mt.createTables(TablesDefinition.personTable);
+        mt.createTables(TablesDefinition.emailTable);
+        mt.createTables(TablesDefinition.phoneTable);
+        mt.createTables(TablesDefinition.registerTable);
+        mt.createTables(TablesDefinition.userTable);
+        mt.createTables(TablesDefinition.groupTable);
+        mt.createTables(TablesDefinition.userGroupTable);
+        mt.createTables(TablesDefinition.registerGroupTable);
+        mt.closeStmt();
         conn.disconnect();
     }
     
