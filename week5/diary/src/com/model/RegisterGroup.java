9d5d79566a767d4185f1ae4b473a01419237e8d8
cjgc <chjugomez@utp.edu.co>
2018-08-02 10:43:00 -0500

    RegisterGroup bean

---
+++ week5/diary/src/com/model/RegisterGroup.java
@@ -0,0 +1,34 @@
+package com.model;
+/**
+ *
+ * @author cj
+ */
+public class RegisterGroup {
+    private int registerGroup_id;
+    private int register_id;
+    private int group_id;
+
+    public int getRegisterGroup_id() {
+        return registerGroup_id;
+    }
+
+    public void setRegisterGroup_id(int registerGroup_id) {
+        this.registerGroup_id = registerGroup_id;
+    }
+
+    public int getRegister_id() {
+        return register_id;
+    }
+
+    public void setRegister_id(int register_id) {
+        this.register_id = register_id;
+    }
+
+    public int getGroup_id() {
+        return group_id;
+    }
+
+    public void setGroup_id(int group_id) {
+        this.group_id = group_id;
+    }
+}
