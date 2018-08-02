c57d045b1916656d80b70713f31065fff7cfc9a7
cjgc <chjugomez@utp.edu.co>
2018-08-02 18:09:32 -0500

    Controller class

---
+++ week5/diary/src/com/controllers/Controllers.java
@@ -0,0 +1,19 @@
+package com.controllers;
+import java.util.HashMap;
+/**
+ *
+ * @author cj
+ */
+public abstract class Controllers {
+    
+    protected HashMap objects;
+    
+    Controllers() {
+        objects = new HashMap();
+    }
+    
+    public abstract void create(String[] content);
+    public abstract void delete(int id);
+    public abstract void edit(int id, String[] content);
+    
+}
