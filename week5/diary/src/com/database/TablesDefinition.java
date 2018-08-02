65b64926d8f31755d5d2658d31bab99a0253925e
cjgc <chjugomez@utp.edu.co>
2018-08-02 12:05:14 -0500

    SQL statements for the tables creation were updated

--- week5/diary/src/com/database/TablesDefinition.java
+++ week5/diary/src/com/database/TablesDefinition.java
@@ -5,12 +5,14 @@ package com.database;
  */
 public class TablesDefinition {
     
-    public static final String countryTable = "CREATE TABLE Country "
+    public static final String countryTable = 
+        "CREATE TABLE IF NOT EXISTS Country "
         + "(country_id INTEGER NOT NULL AUTO_INCREMENT, "
         + " name VARCHAR(100) NOT NULL, "
         + " PRIMARY KEY ( country_id ))";
     
-    public static final String provinceTable = "CREATE TABLE Province "         
+    public static final String provinceTable = 
+        "CREATE TABLE IF NOT EXISTS Province "         
         + "(province_id INTEGER NOT NULL AUTO_INCREMENT, "
         + " name VARCHAR(100) NOT NULL, "
         + " country_id INTEGER NOT NULL, "
@@ -18,7 +20,8 @@ public class TablesDefinition {
         + " CONSTRAINT FOREIGN KEY (country_id) REFERENCES Country (country_id)"
         + " ON DELETE CASCADE ON UPDATE CASCADE)";
     
-    public static final String cityTable = "CREATE TABLE City "
+    public static final String cityTable = 
+        "CREATE TABLE IF NOT EXISTS City "
         + "(city_id INTEGER NOT NULL AUTO_INCREMENT, "
         + " name VARCHAR(100) NOT NULL, "
         + " province_id INTEGER NOT NULL, "
@@ -26,7 +29,8 @@ public class TablesDefinition {
         + " CONSTRAINT FOREIGN KEY (province_id) REFERENCES Province "
         + " (province_id) ON DELETE CASCADE ON UPDATE CASCADE)";
 
-    public static final String addressTable = "CREATE TABLE Address "
+    public static final String addressTable = 
+        "CREATE TABLE IF NOT EXISTS Address "
         + "(address_id INTEGER NOT NULL AUTO_INCREMENT, "
         + " name VARCHAR(150) NOT NULL, "
         + " city_id INTEGER NOT NULL, "
@@ -34,7 +38,8 @@ public class TablesDefinition {
         + " CONSTRAINT FOREIGN KEY (city_id) REFERENCES City "
         + " (city_id) ON DELETE CASCADE ON UPDATE CASCADE)";
     
-    public static final String personTable = "CREATE TABLE Person "         
+    public static final String personTable = 
+        "CREATE TABLE IF NOT EXISTS Person "         
         + "(person_id INTEGER NOT NULL AUTO_INCREMENT, "
         + " name VARCHAR(50) NOT NULL, "
         + " surname VARCHAR(50) NOT NULL, "
@@ -46,7 +51,8 @@ public class TablesDefinition {
         + " CONSTRAINT FOREIGN KEY (address_id) REFERENCES Address "
         + " (address_id) ON DELETE SET NULL ON UPDATE CASCADE)";
     
-    public static final String emailTable = "CREATE TABLE Email "
+    public static final String emailTable = 
+        "CREATE TABLE IF NOT EXISTS Email "
         + "(email_id INTEGER NOT NULL AUTO_INCREMENT, "
         + " name VARCHAR(50) NOT NULL, "
         + " person_id INTEGER NOT NULL, "
@@ -54,7 +60,8 @@ public class TablesDefinition {
         + " CONSTRAINT FOREIGN KEY (person_id) REFERENCES Person "
         + " (person_id) ON DELETE CASCADE ON UPDATE CASCADE)";
         
-    public static final String phoneTable = "CREATE TABLE Phone "
+    public static final String phoneTable = 
+        "CREATE TABLE IF NOT EXISTS Phone "
         + "(phone_id INTEGER NOT NULL AUTO_INCREMENT, "
         + " number VARCHAR(20) NOT NULL, "
         + " countryCode VARCHAR(4) NOT NULL, "
@@ -63,7 +70,8 @@ public class TablesDefinition {
         + " CONSTRAINT FOREIGN KEY (person_id) REFERENCES Person "
         + " (person_id) ON DELETE CASCADE ON UPDATE CASCADE)";
     
-    public static final String registerTable = "CREATE TABLE Register "
+    public static final String registerTable = 
+        "CREATE TABLE IF NOT EXISTS Register "
         + "(register_id INTEGER NOT NULL AUTO_INCREMENT, "
         + " nickname VARCHAR(50) NOT NULL, "
         + " person_id INTEGER NOT NULL, "
@@ -71,7 +79,8 @@ public class TablesDefinition {
         + " CONSTRAINT FOREIGN KEY (person_id) REFERENCES Person "
         + " (person_id) ON DELETE CASCADE ON UPDATE CASCADE)";
         
-    public static final String userTable = "CREATE TABLE User "
+    public static final String userTable = 
+        "CREATE TABLE IF NOT EXISTS User "
         + "(user_id INTEGER NOT NULL AUTO_INCREMENT, "
         + " nickname VARCHAR(50) NOT NULL, "
         + " password VARCHAR(16) NOT NULL, "
@@ -80,14 +89,16 @@ public class TablesDefinition {
         + " CONSTRAINT FOREIGN KEY (person_id) REFERENCES Person "
         + " (person_id) ON DELETE CASCADE ON UPDATE CASCADE)";
         
-    public static final String groupTable = "CREATE TABLE _Group "
+    public static final String groupTable = 
+        "CREATE TABLE IF NOT EXISTS _Group "
         + "(group_id INTEGER NOT NULL AUTO_INCREMENT, "
         + " name VARCHAR(50) NOT NULL, "
         + " photo VARCHAR(500), "
         + " maxAllowed INTEGER NOT NULL, "
         + " PRIMARY KEY ( group_id ))";
         
-    public static final String registerGroupTable = "CREATE TABLE RegisterGroup "
+    public static final String registerGroupTable = 
+        "CREATE TABLE IF NOT EXISTS RegisterGroup "
         + "(registerGroup_id INTEGER NOT NULL AUTO_INCREMENT, "
         + " group_id INTEGER NOT NULL, "
         + " register_id INTEGER NOT NULL, "
@@ -97,7 +108,8 @@ public class TablesDefinition {
         + " CONSTRAINT FOREIGN KEY (register_id) REFERENCES Register"
         + " (register_id) ON DELETE CASCADE ON UPDATE CASCADE)";
         
-    public static final String userGroupTable = "CREATE TABLE UserGroup "
+    public static final String userGroupTable = 
+        "CREATE TABLE IF NOT EXISTS UserGroup "
         + "(userGroup_id INTEGER NOT NULL AUTO_INCREMENT, "
         + " group_id INTEGER NOT NULL, "
         + " user_id INTEGER NOT NULL, "
